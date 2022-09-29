7b26fb07abec52e2309c03af7aa56bb749e9d884
bryancurtis <curtispro97@gmail.com>
2022-09-29 13:43:33 -0400

    adds basic genre chart functionality

--- public/scripts/apiStuff.js
+++ public/scripts/apiStuff.js
@@ -47,7 +47,79 @@ if (access_token) {
         success: function(response) {
             console.log(response);
 
+            // Get Average Popularity
+            let popularity = 0;
+            console.log(response.item);
+            for (let i = 0; i < 20; i++) {
+                popularity += response.items[i].popularity;
+            }
+            popularity = popularity / 20;
+            console.log(popularity);
 
+            // Genres
+            let genres = [];
+            for (let i = 0; i < 20; i++) {
+                for (let j = 0; j < response.items[i].genres.length; j++) {
+                    genres.push(response.items[i].genres[j]);
+                }
+            }
+
+            const genreStats = {};
+            for (const genre of genres) {
+                genreStats[genre] = genreStats[genre] ? genreStats[genre] + 1 : 1;
+            }
+
+            let mostGenres = Object.values(genreStats).sort().reverse().splice(0, 5);
+
+
+            const topGenres = {}
+            for (const genre in genreStats) {
+                for (let i = 0; i < 5; i++) {
+                    if (genreStats[genre] === mostGenres[i]) {
+                        topGenres[genre] = mostGenres[i];
+                    }
+                }
+            }
+            console.log(topGenres);
+
+
+
+
+            // console.log(genreStats);
+            // for (const genre in genreStats) {
+            //     console.log(genreStats[genre]);
+            //     if (genreStats[genre] < 3) delete genreStats[genre];
+            // }
+            
+
+
+            // console.log(genreStats);
+            // console.log(genres);
+            // console.log(Object.keys(genreStats));
+
+            const genreChart = function genreChart() {
+                const ctx = document.getElementById('genreChart').getContext('2d');
+            
+                const genreChart = new Chart(ctx, {
+                    type: 'doughnut',
+                    data: {
+                        labels: Object.keys(topGenres),
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: Object.values(topGenres),
+                            backgroundColor: [
+                                'rgb(255, 99, 132)',
+                                'rgb(54, 162, 235)',
+                                'rgb(255, 205, 86)',
+                                'rgb(15, 208, 102)',
+                                'rgb(130, 110, 230)'
+                            ],
+                            hoverOffset: 4
+                        }]
+                    },
+                });
+            }
+            genreChart();
         }
     });
 
@@ -72,31 +144,7 @@ if (access_token) {
     $('#loggedin').hide();
 }
 
-const genreChart = function genreChart() {
-    const ctx = document.getElementById('genreChart').getContext('2d');
 
-    const genreChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: [
-              'Red',
-              'Blue',
-              'Yellow'
-            ],
-            datasets: [{
-              label: 'My First Dataset',
-              data: [300, 50, 100],
-              backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-              ],
-              hoverOffset: 4
-            }]
-          },
-      });
-}
-genreChart();
 
 
 
